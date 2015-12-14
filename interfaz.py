bd82383bbbeb8d4f8c4780e22cf80a376d72fadd
ezequiel <ezequielbrrt@gmailcom>
2015-12-13 19:23:59 -0600

    Cambios en el archivo

--- interfaz.py
+++ interfaz.py
@@ -10,6 +10,7 @@ from Tkinter import *
 import tkMessageBox
 import subprocess
 
+
 def simularAutomata():
 	if var.get() == 1 or var.get() == 2:
 		poblacion.configure(state='normal')
@@ -33,16 +34,9 @@ def graficarPoblacion():
 	poblacion.configure(state='disabled')
 	programa = subprocess.Popen([sys.executable, 'grafica.py'])
 	 
-def graficarAtractor():
-	atractor.configure(state='disabled')
-	programa = subprocess.Popen([sys.executable, 'graficaAtractor.py'])
-
-def reiniciar():
-	poblacion.configure(state='disabled')
-	atractor.configure(state='disabled')
-	reinicio.configure(state='disabled')
-	simulacion.configure(state='normal')
-	#tkMessageBox.showwarning("Advertencia", "Cerrar ventanas antes de simular otro automata")
+def iniciar():
+	poblacion.configure(state='normal')
+	pass
 
 
 root = Tk()
@@ -53,75 +47,26 @@ var  = IntVar()
 v = StringVar()
 varAtractor = IntVar()
 
-
-w = Scale(root, from_=0, to=100, orient=HORIZONTAL)
-w.grid(row=1,column=1)
-
-
-
-
-
-#Opciones de las densidades de poblacion
-opciones = [
-	   "5%",
-	   "10%",
-	   "25%",
-	   "50%",
-	   "70%",
-	   "80%",
-	   "95%"
-]
-opcionesTiempo = [
-	"0.1",
-	"0.5",
-	"1",
-	"2"
-]
-opcionesSimulacion = [
-	"Indefinido",
-	"2",
-	"5",
-	"10"
-]
-
-
-
+#Label principal
 Label(root, text="Propagación del Dengue en México").grid(row=0,column=1)
 
-#Menu de opciones para las densidades de poblacion
-Label(root,text="Niveles de densidad de Poblacion.").grid(row=3,column=1)
-variable.set(opciones[3]) 
-w = apply(OptionMenu, (root, variable) + tuple(opciones)).grid(row=4,column=1)
+#labels
+Label(root, text="Nivel de Resistencia").grid(row=1,column=0)
+Label(root, text="Velocidad de Simulación").grid(row=1,column=2)
+Label(root, text="Porcentaje de Personas Infectadas").grid(row=1,column=1)
 
-#opciones de Atractor
-Label(root, text="Analisís de Atractor:").grid(row=6,column=0)
-Radiobutton(root, text="3x3", var=varAtractor, value=1 ).grid(row=7,column=0)
-Radiobutton(root, text="4x4", var=varAtractor, value=2 ).grid(row=8,column=0)
-
-#tamaño del cuadrado del automata
-Label(root,text="Tamaño de la simulación:").grid(row=5,column=1)
-E1 = Entry(root,textvariable=v).grid(row=6,column=1)
-v.set("50")
-Label(root,text=v.get()+" x "+v.get()+" Cuadros").grid(row=6,column=2)
-
-#Tiempo de rapidez de la simulación
-Label(root,text="Rapidéz de Simulación").grid(row=7,column=1)
-variableTiempo.set(opcionesTiempo[2]) 
-w = apply(OptionMenu, (root, variableTiempo) + tuple(opcionesTiempo)).grid(row=8,column=1)
-Label(root,text="Segundos").grid(row=8,column=2)
-
-#Número de simulaciones
-Label(root,text="Número de Simuaciones").grid(row=9,column=1)
-variableSimulacion.set(opcionesSimulacion[2]) 
-w = apply(OptionMenu, (root, variableSimulacion) + tuple(opcionesSimulacion)).grid(row=10,column=1)
-Label(root,text="Simulaciones").grid(row=10,column=2)
+#scrollbar's
+resistencia = Scale(root, from_=0, to=100, orient=HORIZONTAL)
+resistencia.grid(row=2,column=0)
+velocidad = Scale(root, from_=1, to=10, orient=HORIZONTAL)
+velocidad.grid(row=2,column=2)
+personas = Scale(root, from_=1, to=10, orient=HORIZONTAL)
+personas.grid(row=2,column=1)
 
 #Botones para simular y graficar
+iniciar = Button(root, text="Iniciar Simulación",command=iniciar, relief=GROOVE)
 poblacion = Button(root, text="Graficar Población", command=graficarPoblacion,state='disabled',relief=GROOVE)
-simulacion = Button(root, text="Simular Automata", command=simularAutomata)
-atractor = Button(root, text="Graficar Atractor", command=graficarAtractor,state='disabled')
 
-poblacion.grid(row=11,column=0)
-simulacion.grid(row=11,column=1)
-atractor.grid(row=11,column=2)
+iniciar.grid(row=11,column=1)
+poblacion.grid(row=12,column=1)
 mainloop()
