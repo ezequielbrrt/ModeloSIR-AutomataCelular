1d4bf928fbbbcbbaf22f5dab09f95cd71a5fabe2
ezequiel <ezequielbrrt@gmailcom>
2015-12-21 21:10:52 -0600

    nueva interfaz

--- interfaz.py
+++ interfaz.py
@@ -21,16 +21,17 @@ def iniciar():
 	datosResistencia = resistencia.get()
 	datosVelocidad = velocidad.get()
 	datosPersonas = personas.get()
-	#mapa = subprocess.Popen([sys.executable, 'mapa.py'])
-	print datosResistencia
-	print datosVelocidad
-	print datosPersonas
+	mapa = subprocess.Popen([sys.executable, 'mapa.py'])
 	datos = open("Datos.txt","w")
 	datos.write(str(datosResistencia)+","
 		+str(datosVelocidad)+","+str(datosPersonas))
 	datos.close()
 
+#configuraciones de la ventana principal
 root = Tk()
+root.geometry("300x400")
+root.resizable(width=False, height=False)
+
 variable = StringVar(root)
 variableTiempo = StringVar(root)
 variableSimulacion = StringVar(root)
@@ -40,25 +41,35 @@ varAtractor = IntVar()
 
 #Label principal
 Label(root, text="Propagación del Dengue en México").grid(row=0,column=1)
+Label(root, text="           ").grid(row=0,column=0)
+
 
 #labels
-Label(root, text="Nivel de Resistencia").grid(row=1,column=0)
-Label(root, text="Velocidad de Simulación").grid(row=1,column=2)
-Label(root, text="Porcentaje de Personas Infectadas").grid(row=1,column=1)
+
+Label(root, text="").grid(row=2,column=0)
+Label(root, text="Nivel de Resistencia").grid(row=3,column=1)
+Label(root, text="Velocidad de Simulación").grid(row=5,column=1)
+Label(root, text="Porcentaje de Personas Infectadas").grid(row=7,column=1)
 
 #scrollbar's
 resistencia = Scale(root, from_=0, to=100, orient=HORIZONTAL)
-resistencia.grid(row=2,column=0)
+resistencia.grid(row=4,column=1)
 velocidad = Scale(root, from_=1, to=10, orient=HORIZONTAL)
-velocidad.grid(row=2,column=2)
+velocidad.grid(row=6,column=1)
 personas = Scale(root, from_=1, to=10, orient=HORIZONTAL)
-personas.grid(row=2,column=1)
-
+personas.grid(row=8,column=1)
 
 #Botones para simular y graficar
+Label(root, text="").grid(row=9,column=0)
+Label(root, text="").grid(row=10,column=0)
+
 iniciar = Button(root, text="Iniciar Simulación",command=iniciar, relief=GROOVE)
+Label(root, text="").grid(row=12,column=0)
 poblacion = Button(root, text="Graficar Población", command=graficarPoblacion,state='disabled',relief=GROOVE)
+Label(root, text="").grid(row=14,column=0)
+Label(root, text="Barreto Aviles Ezequiel Adrian").grid(row=15,column=1)
+
 
 iniciar.grid(row=11,column=1)
-poblacion.grid(row=12,column=1)
+poblacion.grid(row=13,column=1)
 mainloop()
